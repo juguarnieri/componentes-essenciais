e882751e45ed2d5fdf04a09a7b829227439d7232
juguarnieri <juliaguarnieri04@gmail.com>
2025-03-25 19:22:42 -0300

    feat: create page with basic components and css

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React from 'react';
+import { StyleSheet, Text, View, Image} from 'react-native';
+export default function Home() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>A natureza é o único livro que oferece um conteúdo valioso em todas as suas folhas.</Text>
+      <Text style={styles.author}>Johann Goethe</Text>
+      <Text style={styles.ask}>Reflita sobre a natureza!</Text>
+      <Image source={require('./public/nature.png')} style={styles.image}/>
     </View>
   );
 }
@@ -13,8 +14,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e0f7fa',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#00796b',
+    marginBottom: 10,
+  },
+  author: {
+    fontSize: 16,
+    color: 'red',
+    padding: 10,
+    fontStyle: 'italic',
+    textAlign: 'right',
+    alignSelf: 'flex-end',
+    marginBottom: 20,
+  },
+  ask: {
+    fontSize: 18,
+    color: 'blue',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 50,
+    marginBottom: 10,
   },
 });
+
